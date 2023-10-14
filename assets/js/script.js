496131a17a0d97832eae985719c159feec85a48e
Amirmohammad Masoumi <39368982+masoomi1396@users.noreply.github.com>
2023-10-14 20:16:39 +0330

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-const { TIMEOUT } = require("mz/dns");
-
 const searchInput = document.querySelector('#searchBox input');
 const searchButton = document.querySelector('#searchBox button');
 const searchBox = document.querySelector('#searchBox');
@@ -14,7 +12,8 @@ searchButton.addEventListener('click', () => {
     const city = searchInput.value;
     if (city === '')
         return;
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response=>response.json()).then(json =>{
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+        console.log("It is true")
         if (json.cod == '404') {
             weatherBox.classList.remove('fade-in');
             weatherBox.classList.add('hidden');
@@ -26,19 +25,20 @@ searchButton.addEventListener('click', () => {
         weatherBoxNotFind.classList.add('hidden');
         weatherBoxNotFind.classList.remove('fade-in');
         searchBox.classList.remove('h-[705px]');
-        weatherBox.classList.add('hidden');
-        
-        
+        // weatherBox.classList.add('hidden');
+
+
 
         const temprature = document.querySelector('#temprature');
         const detail = document.querySelector('#detail');
 
-        switch (json.weather[0].main)
-        {
+        switch (json.weather[0].main) {
             case 'Clear':
                 weatherBoxImage.src = 'assets/image/clear.png';
                 break;
-
+            case 'Snow':
+                weatherBoxImage.src = 'assets/image/snow.png';
+                break;
             case 'Rain':
                 weatherBoxImage.src = 'assets/image/rain.png';
                 break;
